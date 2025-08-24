d04c7952c11cfc98ed06071d6444d4aa0d118d0c
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-08-24 21:23:52 +0300

    fix(hyprland): race condition for starting hyprpaper

--- scripts/hyprpaper-random.sh
+++ scripts/hyprpaper-random.sh
@@ -5,4 +5,4 @@ CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
-hyprctl hyprpaper reload ,"$WALLPAPER"
+while ! hyprctl hyprpaper reload ,"$WALLPAPER"; do sleep 1; done
